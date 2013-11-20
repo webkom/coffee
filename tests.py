838259f8508750664327ac81fac2057ad732a2a8
Rolf Erik Lekang <me@rolflekang.com>
2013-11-21 00:39:07 +0100

    add test sceleton

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestViews(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+        unittest.main()
