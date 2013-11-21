9ea611ff839cb6e7cee68bc20a953017b12456be
Rolf Erik Lekang <me@rolflekang.com>
2013-11-21 12:07:38 +0100

    add configuration files

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 setup:
 	virtualenv venv
 	venv/bin/pip install -r requirements.txt
+	cp example_config .coffeerc
 
 run:
 	venv/bin/python server.py
---
+++ config.py
@@ -0,0 +1,17 @@
+import ConfigParser
+
+from redis import ConnectionPool
+
+
+config = ConfigParser.ConfigParser()
+config.read(['.coffeerc'])
+
+pool = ConnectionPool(
+    db=config.get('coffee_server', 'redis_db'),
+    host=config.get('coffee_server', 'redis_host'),
+    port=config.get('coffee_server', 'redis_port')
+)
+
+app_config = {
+    'DEBUG': config.getboolean('coffee_server', 'debug')
+}
