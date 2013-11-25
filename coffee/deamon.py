24111bfa7569e0d41a932567f63929d5c5a8e875
Rolf Erik Lekang <me@rolflekang.com>
2013-11-25 20:13:56 +0100

    append coffee to sys.path

--- coffee/deamon.py
+++ coffee/deamon.py
@@ -2,7 +2,7 @@ import time
 import sys
 import os
 
-sys.path(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from coffee.config import pool
 from coffee.models import Status
