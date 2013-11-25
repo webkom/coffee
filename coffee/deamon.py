9e2441e05c396f4c5c541d4fc18c86db2c4dd12e
Rolf Erik Lekang <me@rolflekang.com>
2013-11-25 16:22:29 +0100

    make deamon run if the db is empty

--- coffee/deamon.py
+++ coffee/deamon.py
@@ -1,31 +1,18 @@
 from datetime import datetime
 import time
-import RPi.GPIO as GPIO
 import redis
 
 from coffee.config import app_config, pool
 
 DEBUG = 1
-GPIO.setmode(GPIO.BCM)
-PIN = app_config['gpio_pin']
+PIN = 14
 
 r = redis.Redis(connection_pool=pool)
 
 
-def rc_time(RCpin):
-    reading = 0
-    GPIO.setup(RCpin, GPIO.OUT)
-    GPIO.output(RCpin, GPIO.LOW)
-    time.sleep(0.1)
-
-    GPIO.setup(RCpin, GPIO.IN)
-    while (GPIO.input(RCpin) == GPIO.LOW):
-        reading += 1
-    return reading
-
-
 def get_last_start(previous, is_on):
-    if is_on and not previous['status'] and datetime.now() > datetime.strptime('%Y-%m-%d %H:%M'):
+    if is_on and not previous['status'] and\
+       datetime.now() > datetime.strptime(previous['last_start'], '%Y-%m-%d %H:%M'):
         return datetime.now().strftime('%Y-%m-%d %H:%M')
     else:
         return previous['last_start']
@@ -33,6 +20,12 @@ def get_last_start(previous, is_on):
 
 def update_status(is_on):
     previous = r.hgetall('coffeestatus')
+    if not 'status' in previous:
+        previous = {
+            'status': False,
+            'last_start': '1977-11-21 12:00'
+        }
+
     if not previous == is_on:
         r.hmset('coffeestatus', {
             'status': is_on,
@@ -40,8 +33,23 @@ def update_status(is_on):
         })
 
 
-while True:
-    if rc_time(PIN) < 1500:
-        update_status(True)
-    else:
-        update_status(False)
+def main():
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+
+    def rc_time(RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.1)
+
+        GPIO.setup(RCpin, GPIO.IN)
+        while (GPIO.input(RCpin) == GPIO.LOW):
+            reading += 1
+        return reading
+
+    while True:
+        if rc_time(PIN) < 1500:
+            update_status(True)
+        else:
+            update_status(False)
