3ac0deb7a51043b706ec242bad10a2be77794983
Mats Byrkjeland <matsbyr@gmail.com>
2016-10-18 22:49:21 +0200

    Set default server host address to 127.0.0.1

--- coffee/config.py
+++ coffee/config.py
@@ -6,6 +6,6 @@ app_config = {
     'REDIS_HOST': os.getenv('REDIS_HOST', '127.0.0.1'),
     'REDIS_PORT': int(os.getenv('REDIS_PORT', 6379)),
     'REDIS_PW': os.getenv('REDIS_PW', None),
-    'SERVER_HOST': os.getenv('SERVER_HOST', '0.0.0.0'),
+    'SERVER_HOST': os.getenv('SERVER_HOST', '127.0.0.1'),
     'SERVER_PORT': int(os.getenv('SERVER_PORT', 5000))
 }
