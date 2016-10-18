a38f6dbb9d1f433aa1cde6fe4b9d0718f57038d1
Mats Byrkjeland <matsbyr@gmail.com>
2016-10-19 01:44:27 +0200

    Add long sleep with LED off after button press

--- coffee/deamon.py
+++ coffee/deamon.py
@@ -1,15 +1,7 @@
-#!/home/pi/coffee/venv/bin/python
-
 import os
-import sys
 import time
 
 from coffee.models import Status
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-
-DEBUG = 1
 
 # The GPIO pin the button is connected to
 BUTTON_PIN = 7
@@ -26,22 +18,25 @@ def main():
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(LED_PIN, GPIO.OUT, initial=1)
 
-    # Blink the LED, leave off
+    # Blink the LED, leave in same state as when called
     def blink_led(num_blinks, blink_duration, blink_pause):
+        initial = GPIO.input(LED_PIN)
         for b in range(num_blinks):
-            GPIO.output(LED_PIN, 1)
+            GPIO.output(LED_PIN, not initial)
             time.sleep(blink_duration)
-            GPIO.output(LED_PIN, 0)
+            GPIO.output(LED_PIN, initial)
             time.sleep(blink_pause)
-        GPIO.output(LED_PIN, 0)
 
     # Listen for button presses
     while True:
         input_value = GPIO.input(BUTTON_PIN)
         if input_value == False:
             status.update(True)
-            blink_led(3, 0.3, 0.2)
+            blink_led(3, 0.3, 0.2) # equals sleep of 3 * (0.3 + 0.2) = 1.5 seconds
+            GPIO.output(LED_PIN, 0) # Turn off light to signalize button cannot be pushed
+            time.sleep(300) # Sleep for five minutes to avoid spamming the button
             status.update(False)
+            GPIO.output(LED_PIN, 1) # Turn on light when button is ready to be pushed again
         time.sleep(0.1)
 
 main()
