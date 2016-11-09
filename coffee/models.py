58a04466093354b0fdba47e30afef02f4334cfbe
Mats Byrkjeland <matsbyr@gmail.com>
2016-11-10 00:00:46 +0100

    Use UTC times and format as ISO8601 strings

--- coffee/models.py
+++ coffee/models.py
@@ -22,7 +22,7 @@ class Status (object):
         except:
             self.current_status = False
             self.last_start = datetime.strptime('1977-11-21 12:00', '%Y-%m-%d %H:%M')
-            span = datetime.now() - self.last_start
+            span = datetime.utcnow() - self.last_start
             self.hours_since = (span.days*24)+(span.seconds//3600)
             self.minutes_since = (span.seconds//60) % 60
 
@@ -33,14 +33,14 @@ class Status (object):
         previous = self.redis.hgetall('coffeestatus')
         self.current_status = previous['status'] == 'True'
         self.last_start = datetime.strptime(previous['last_start'], '%Y-%m-%d %H:%M')
-        span = datetime.now() - self.last_start
+        span = datetime.utcnow() - self.last_start
         self.hours_since = (span.days*24)+(span.seconds//3600)
         self.minutes_since = (span.seconds//60) % 60
 
     def to_dict(self):
         return {
             'status': self.current_status,
-            'last_start': self.last_start.strftime('%Y-%m-%d %H:%M'),
+            'last_start': self.last_start.isoformat(),
             'time_since': {
                 'hours': self.hours_since,
                 'minutes': self.minutes_since
@@ -48,19 +48,20 @@ class Status (object):
         }
 
     def calculate_last_start(self, status):
-        if status and datetime.now() > self.last_start:
-            return datetime.now()
+        if status and datetime.utcnow() > self.last_start:
+            return datetime.utcnow()
         else:
             return self.last_start
 
     def update(self, new_status):
-        if not self.current_status == new_status:
-            self.current_status = new_status
-            if self.last_start + timedelta(seconds=40) < datetime.now():
-                self.log_status(new_status)
-                self.notify_integrations()
-            self.last_start = self.calculate_last_start(new_status)
-            self.save()
+        if self.current_status == new_status:
+            return
+        self.current_status = new_status
+        if self.last_start + timedelta(seconds=40) < datetime.utcnow():
+            self.log_status(new_status)
+            self.notify_integrations()
+        self.last_start = self.calculate_last_start(new_status)
+        self.save()
 
     def notify_integrations(self):
         for integration in self.integrations:
@@ -68,10 +69,10 @@ class Status (object):
 
     def log_status(self, status):
         if status:
-            self.redis.hincrby('coffeestats', datetime.now().strftime('%Y-%m-%d'), 1)
+            self.redis.hincrby('coffeestats', datetime.utcnow().isoformat(), 1)
 
     def get_count(self, date):
-        return self.redis.hget('coffeestats', date.strftime('%Y-%m-%d')) or 0
+        return self.redis.hget('coffeestats', date.isoformat()) or 0
 
     def get_stats(self):
         return self.redis.hgetall('coffeestats')
